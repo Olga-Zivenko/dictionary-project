f2ff97d99c46a6692a1691efb9c1c243911d5be6
Olga-Zivenko <108621714+Olga-Zivenko@users.noreply.github.com>
2022-10-09 23:44:15 +0200

    add Dictionary form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./logo.png"
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo}  alt="logo" className="App-logo img-fluid" />
        
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by SheCodes</footer>
+    </div>
     </div>
   );
 }
