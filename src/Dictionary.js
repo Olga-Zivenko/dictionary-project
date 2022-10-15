1d1de6d43b1a10248b394b499ff84931c24c7345
Olga-Zivenko <108621714+Olga-Zivenko@users.noreply.github.com>
2022-10-15 23:37:23 +0200

    rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,21 @@
 import React, {useState} from "react";
 import axios from "axios";
 import  "./Dictionary.css";
+import Results from "./Results";
 
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults]= useState(null);
+    
     function handleResponse(response) {
-        console.log(response);
-    }
+         console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
+        }
     function search(event){
         event.preventDefault();
         alert(`Searching for ${keyword}`);
-        let apiUrl="https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event){
@@ -22,7 +26,7 @@ export default function Dictionary() {
 <form onSubmit={search}>
     <input type="search" autoFokus={true} onChange={handleKeywordChange} />
 </form>
-
+<Results results={results} />
 
     </div>);
 }
